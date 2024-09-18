93f7b8f675bed7b09c6ed22b7dd55d9f4573cfcd
bhq628@myy.haaga-helia.fi <bhq628@myy.haaga-helia.fi>
2024-09-18 13:27:34 +0300

    Kansion Luonti T05

---
+++ Teht_05_01/Luento_05.js
@@ -0,0 +1,17 @@
+//Tämä on kommenttirivi. Luodaan muuttuja keskusmusitiin,
+//JavaScript ns. Globaaliin NameSpace:iin: 
+var lastName;
+lastName = "Mattila"; //String
+
+let pisteet = 25; //Number
+var paino = 80;   //Number
+
+let pvm = "18.9.2024"; //Date
+
+let n1 = 5;
+let n2 = 7.1;
+let summa = n1 + n2; //12.1
+
+let sukunimi = "Milla";
+
+let kokonimi = etunimi + " " + sukunimi;
