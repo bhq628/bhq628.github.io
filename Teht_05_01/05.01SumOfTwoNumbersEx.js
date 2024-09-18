b231b34a72424511254ff6a3ed63e0209d06baf7
bhq628@myy.haaga-helia.fi <bhq628@myy.haaga-helia.fi>
2024-09-18 13:35:00 +0300

    Valmis työ

---
+++ Teht_05_01/05.01SumOfTwoNumbersEx.js
@@ -0,0 +1,7 @@
+// TODO: JavaScript code
+// Define, luo muuttuja:
+//Assign, (sijoita) tallenna data-arvo muuttujaan
+let number1 = 3;
+let number2 = 4;
+let sum = number1 + number2;
+console.log(sum);
