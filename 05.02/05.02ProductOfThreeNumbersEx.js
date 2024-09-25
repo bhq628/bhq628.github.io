67822d8b47530e476c7339f1fe94e847c7c2982a
bhq628@myy.haaga-helia.fi <bhq628@myy.haaga-helia.fi>
2024-09-25 11:19:55 +0300

    finished 05.03

---
+++ 05.02/05.02ProductOfThreeNumbersEx.js
@@ -0,0 +1,4 @@
+// TODO: JavaScript code
+var x = 5;
+var y = 7;
+var c = 10;
---
+++ 05.03/05.03DecimalDivisionEx.js
@@ -0,0 +1,9 @@
+// TODO: JavaScript code
+// var = let. to define a function
+
+let a = 4.87;
+let b = 5.03;
+
+let quotient = a/b;
+
+console.log(quotient);
