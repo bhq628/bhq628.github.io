685bb079a8eac18f59950e1b12ee18d8528fdd94
bhq628@myy.haaga-helia.fi <bhq628@myy.haaga-helia.fi>
2024-09-25 11:56:02 +0300

    05.06 toimiva

--- 05.06/05.06UnitFineEx.js
+++ 05.06/05.06UnitFineEx.js
@@ -1,11 +1,11 @@
 function calculateUnitFine() {
     // Read the value from the input field into a variable
-      let netIncome = document.getElementById("txtNetIncome").Value;
+      let netIncome = document.getElementById("txtNetIncome").value;
 
     // Calculate the unit fine and assign the result into another variable
     let unitFine = (netIncome - 255)/60;
 
     // Write the answer to the web page DOM, in the answer div, as the content
     document.getElementById("answerDiv").innerHTML= "Unit fine is " + unitFine.toFixed(2) + " euros.";
-    
+
 }
